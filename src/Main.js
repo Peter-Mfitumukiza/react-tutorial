b3ed33b6d4dc03b889c352dbc843f13852fee6d2
Mfitumukiza Peter <mfitep6@gmail.com>
2022-02-21 21:08:23 +0200

    initial commit

---
+++ src/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Component } from "react";
+
+const Main = () =>{
+    let message = "Helloo Peter! Long time no see.";
+    return (
+        <div>
+            <h2>{ message }</h2>
+        </div>
+    )
+}
+
+export default Main;
